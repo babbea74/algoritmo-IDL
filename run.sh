96212531addf1e7031dab1dcef27bb70d2f9095b
ATom93 <a.tomasino@studenti.poliba.it>
2018-09-14 11:47:05 +0200

    primo commit

---
+++ run.sh
@@ -0,0 +1,9 @@
+if [ -f /tmp/.X1-lock ]; then
+    rm -f /tmp/.X1-lock
+fi
+if [ -d /tmp/.X11-unix ]; then
+    rm -rf /tmp/.X11-unix
+fi
+vnc4server
+export DISPLAY=$HOSTNAME:1
+idl -vm=CCA_CHANGE_DETECTION_IDL_TOT.sav
