f67dd7d95fbf6dbcd8ccca9485c34e4679149ff5
ATom93 <37183327+ATom93@users.noreply.github.com>
2018-09-15 09:54:57 +0200

    aggiunta stampa della variabile PATH

--- run.sh
+++ run.sh
@@ -6,7 +6,11 @@ if [ -d /tmp/.X11-unix ]; then
 fi
 vnc4server
 export DISPLAY=$HOSTNAME:1
+echo
 echo $DISPLAY
+echo 
+echo $PATH
+echo
 idl -vm=CCA_CHANGE_DETECTION_IDL_TOT.sav &
 xte 'key Return'
 sleep 6
