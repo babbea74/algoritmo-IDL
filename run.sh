fb13cb3616ed01ef98a87c609ed06ba515c20f55
ATom93 <37183327+ATom93@users.noreply.github.com>
2018-09-16 13:01:31 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,6 +8,7 @@ vnc4server
 export DISPLAY=$HOSTNAME:1
 export PATH=$PATH:/usr/local/exelis/idl85/bin
 idl -vm=CCA_CHANGE_DETECTION_IDL_TOT.sav &
+sleep 10 
 xte 'key Return'
 echo
 echo 'primo invio'
