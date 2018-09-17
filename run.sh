9d3d222982b72eba50c4c82c774901f730c570c6
ATom93 <37183327+ATom93@users.noreply.github.com>
2018-09-17 11:25:21 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,14 +7,8 @@ fi
 vnc4server
 export DISPLAY=$HOSTNAME:1
 export PATH=$PATH:/usr/local/exelis/idl85/bin
-echo '_____________________________________________'
-echo 'DIRECTORY ROOT'
-ls -al /
-echo '_____________________________________________'
-echo 'DIRECTORY CORRENTE'
-ls -al 
-echo '_____________________________________________'
+mv ./T1_data.tif /
+mv ./T2_data.tif /
 xte 'sleep 30' 'key Return' &
-xte 'sleep 60' 'key Return' &
 idl -vm=CCA_CHANGE_DETECTION_IDL_TOT.sav 
 
