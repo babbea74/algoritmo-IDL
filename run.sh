aa11b235aad51103932ee4b7c0fbf3095d7dc318
ATom93 <37183327+ATom93@users.noreply.github.com>
2018-09-17 10:19:23 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,5 +8,6 @@ vnc4server
 export DISPLAY=$HOSTNAME:1
 export PATH=$PATH:/usr/local/exelis/idl85/bin
 xte 'sleep 30' 'key Return' &
+xte 'sleep 60' 'key Return' &
 idl -vm=CCA_CHANGE_DETECTION_IDL_TOT.sav 
 
